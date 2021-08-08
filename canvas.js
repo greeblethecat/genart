a76c6d1bdcd10bb2e8d4e98e4f28d030dae94797
greeble <greeblethecat@gmail.com>
2021-08-07 23:03:31 -0700

    basic p5 workflow

--- canvas.js
+++ canvas.js
@@ -1,40 +1,85 @@
+/*
+ * Build script for the project. Generates the docs dir from views
+ *
+ * Kinda like a janky gradle
+ */
+
 const fs = require('fs')
+const fsPromises = require('fs/promises')
 const ejs = require('ejs')
 
-// Load all of the Project instances from projects/
-const projects = fs.readdirSync('projects').map(proj => {
-  let project = require(`./projects/${proj}/${proj}.js`)
-  project.name = proj
-  return project
-})
+const VIEW_DIR = `${__dirname}/view`
+const VIEW_JS_DIR = `${VIEW_DIR}/js`
+const VIEW_VENDOR_DIR = `${VIEW_JS_DIR}/vendor`
+const VIEW_PIECES_DIR = `${VIEW_JS_DIR}/pieces`
 
-function buildIndex() {
-  return new Promise((res, rej) => {
-    let outputDir = `docs`
-    // If outputDir doesn't exist, then create it
+const OUTPUT_DIR = `${__dirname}/docs`
+const OUTPUT_JS_DIR = `${OUTPUT_DIR}/js`
+const OUTPUT_VENDOR_DIR = `${OUTPUT_JS_DIR}/vendor`
+const OUTPUT_PIECES_DIR = `${OUTPUT_JS_DIR}/pieces`
+
+function createDirIfNotExist(dir) {
     try {
-      fs.accessSync(outputDir)
+      fs.accessSync(dir)
     } catch (err) {
-      fs.mkdirSync(outputDir)
+      fs.mkdirSync(dir)
     }
-    ejs.renderFile(`${__dirname}/lib/views/index.ejs`, {}, {}, (err, str) => {
-      if (err) { rej(err) }
-      fs.writeFile(`${outputDir}/index.html`, str, err => {
-        res()
-        console.log('built index')
+}
+
+async function clean() {
+  //fs.rmdir(OUTPUT_DIR, { recursive: true }, (err) => {
+  //})
+  await fsPromises.rm(OUTPUT_DIR, { recursive: true, force: true })
+  createDirIfNotExist(OUTPUT_DIR)
+}
+
+async function assembleBoilerplate() {
+  createDirIfNotExist(OUTPUT_JS_DIR)
+  createDirIfNotExist(OUTPUT_VENDOR_DIR)
+  createDirIfNotExist(OUTPUT_PIECES_DIR)
+
+  // Copy over the p5.js src
+  createDirIfNotExist(`${OUTPUT_VENDOR_DIR}/p5`)
+  await fsPromises.copyFile(`${VIEW_VENDOR_DIR}/p5/p5.js`, `${OUTPUT_VENDOR_DIR}/p5/p5.js`)
+
+  // Assemble index.ejs page
+  await new Promise((resolve, reject) => {
+    ejs.renderFile(`${VIEW_DIR}/index.ejs`, {}, {}, (err, str) => {
+      if (err) { reject(err) }
+      fs.writeFile(`${OUTPUT_DIR}/index.html`, str, err => {
+        if (err) { reject(err) }
+        resolve()
       })
     })
   })
+
+  console.log('assembled boilerplate')
 }
 
-async function buildAll() {
-  await buildIndex()
-  projects.forEach(proj => proj.build())
-  console.log('ran buildAll')
+async function assemblePieces() {
+  // Load all of the pieces
+  const allPieces = fs.readdirSync(VIEW_PIECES_DIR)
+  allPieces.forEach(async pieceName => {
+    await fsPromises.copyFile(`${VIEW_PIECES_DIR}/${pieceName}`, `${OUTPUT_PIECES_DIR}/${pieceName}`)
+  })
+  console.log(allPieces)
+  console.log(`assembled ${allPieces.length} pieces`)
+}
+
+async function assemble() {
+  await assembleBoilerplate()
+  await assemblePieces()
+  console.log('finished assemble')
+}
+
+async function build() {
+  await clean()
+  await assemble()
+  console.log('finished build')
 }
 
 function defaultTask() {
-  buildAll()
+  build()
 }
 
 // Interpret the command line argument(s), if any
@@ -42,7 +87,15 @@ const TASK_ARG_IDX = 2
 const TASK_ARG = process.argv[TASK_ARG_IDX]
 if (TASK_ARG) {
   switch(TASK_ARG) {
+    case 'clean':
+      clean()
+      break
+    case 'assemble':
+      clean()
+      assemble()
+      break
     case 'build':
+      build()
       break
     default:
       throw new Exception(`Don't know how to run task '${TASK_ARG}'`)
---
+++ docs/js/pieces/0000_staticStarField.js
@@ -0,0 +1,1 @@
+alert('hello from piece 0000')
