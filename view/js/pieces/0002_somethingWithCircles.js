e2b12f77ed75628103c22d7d56f34f9df7c9cf72
greeble <greeblethecat@gmail.com>
2022-02-03 23:40:33 -0800

    0002 - something with circles

--- view/js/pieces/0002_somethingWithCircles.js
+++ view/js/pieces/0002_somethingWithCircles.js
@@ -1,15 +1,33 @@
 const w = window.innerWidth;
 const h = window.innerHeight;
 
-const AllPoints = [];
-let DrawnPoints = [];
-
 function setup() {
   createCanvas(w, h);
 }
 
+let minRings = 8;
+let maxRings = 128;
+let count = minRings;
+let countPingPong = true;
 function draw() {
   background(0, 0, 0);
-  fill('red');
-  circle(0,0, 100);
+  let fromColor = color(0,0,255);
+  let toColor = color(255,0,0); // center of orb
+  if (countPingPong) {
+    count++;
+    if (count > maxRings) {
+      countPingPong = false;
+    }
+  } else {
+    count--;
+    if (count <= minRings) {
+      countPingPong = true;
+    }
+  }
+  for (i = 0; i < count; i++) {
+    let color = lerpColor(fromColor, toColor, i/(count-1));
+    fill(color);
+    stroke('black');
+    circle(w/2,h/2, (count-i)*(w/count));
+  }
 }
